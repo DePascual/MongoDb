c0ca5aee4deea870e2cd920d20d29fb9402008b2
DePascual <carolinadepascualjimenez@gmail.com>
2016-04-27 15:22:42 +0200

    Haciendo conexion

---
+++ Mongo1/Mongo1/Mongo1/Models/usuarios.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mongo1.Models
+{
+    public class usuarios
+    {
+        [BsonId]
+        public ObjectId id { get; set; }
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+    }
+}
